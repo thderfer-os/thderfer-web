ad40c40e6c1840c0f26f8235752252fc49270f74
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2025-01-11 12:46:10 -0800

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using AdminConsole.Models;
+
+namespace AdminConsole.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
